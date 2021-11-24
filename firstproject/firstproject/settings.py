a8affd6b394bc5e1bfdc68570d25414b8370ff37
고도희 <ksder99@likelion.org>
2021-11-24 20:49:45 +0900

    setting to call static files

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'firstapp.apps.FirstappConfig',
+    'imagekit',
 ]
 
 MIDDLEWARE = [
@@ -123,8 +124,13 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    os.path.join(BASE_DIR, 'firstapp', 'static'),
+    #os.path.join(BASE_DIR, 'firstapp', 'static'),
+    os.path.join(BASE_DIR, "static")
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- firstproject/firstproject/urls.py
+++ firstproject/firstproject/urls.py
@@ -23,3 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.showIndex, name="index.html")
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
