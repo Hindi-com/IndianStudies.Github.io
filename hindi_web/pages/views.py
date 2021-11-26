2a628ff40cae8a7f224cf4903e2c37790d61fbac
WithKKM <93486282+WithKKM@users.noreply.github.com>
2021-11-26 21:13:25 +0900

    login html css

--- hindi_web/pages/views.py
+++ hindi_web/pages/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import redirect, render
 
 # Create your views here.
+
+
 def home(request):
     return render(request, "index.html")
 
+
+def login(request):
+    return render(request, 'login.html')
+
+
 def introduction(request):
     return render(request, 'introduction-main.html')
+
 
 def community(request):
     return render(request, 'community-main.html')
